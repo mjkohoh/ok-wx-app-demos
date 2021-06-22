3033e0a45da05039ce61d0e068f688a9071a9dd9
mj <mj@kohoh.cn>
2021-06-22 14:58:02 +0800

    code input

--- input/pages/index/index.js
+++ input/pages/index/index.js
@@ -1,3 +1,21 @@
 Page({
-  
+  data: {
+    inputValue: ''
+  },
+  bindKeyInput: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  bindReplaceInput: function (e) {
+    const {
+      value
+    } = e.detail
+    return value.replace(/11/g, '2')
+  },
+  bindHideKeyboard: function (e) {
+    if (e.detail.value === '123') {
+      wx.hideKeyboard()
+    }
+  }
 })
