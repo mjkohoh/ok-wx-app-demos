f8da8f8717c8b25d8ea2b85995a92aa756aaebbd
mj <mj@kohoh.cn>
2021-06-23 14:50:21 +0800

    template and import and include

---
+++ template-and-import-and-include/pages/index/index.js
@@ -0,0 +1,16 @@
+Page({
+  data: {
+    staffA: {
+      firstname: 'Hulk',
+      lastname: 'Hu'
+    },
+    staffB: {
+      firstname: 'Shang',
+      lastname: 'You'
+    },
+    staffC: {
+      firstname: 'Gideon',
+      lastname: 'Lin'
+    },
+  }
+})
