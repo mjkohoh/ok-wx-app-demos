f158ff1505cadbe354b9c6297458077e6518d1ec
mj <mj@kohoh.cn>
2021-06-24 19:54:34 +0800

    code lable

---
+++ label/index/index.js
@@ -0,0 +1,16 @@
+const app = getApp()
+
+Page({
+  data: {
+    items: [{
+        value: 'USA',
+        name: '美国'
+      },
+      {
+        value: 'CHN',
+        name: '中国',
+        checked: true
+      },
+    ]
+  }
+})
